ddd12e9c3ab4a7f3f9ec609141e2496db1e9ea01
Lilia Lashin <lilial@crow.co.il>
2025-04-21 14:42:06 +0300

    validation method with alert for error

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -45,6 +45,37 @@ class _NewExpense extends State<NewExpense> {
         : formater.format(_selectedDate!);
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount < 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder:
+            (ctx) => AlertDialog(
+              title: const Text('Invalid Input'),
+              content: const Text(
+                'Please make sure a valid title, amount, date and category was entered.',
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                  child: const Text('Ok'),
+                ),
+              ],
+            ),
+      );
+      return;
+    }
+
+    // valid case
+  }
+
   /*
   void _presentDatePicker() {
     final now = DateTime.now();
@@ -137,10 +168,7 @@ class _NewExpense extends State<NewExpense> {
                     child: Text('Cancel'),
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      print(_titleController.text);
-                      print(_amountController.text);
-                    },
+                    onPressed: _submitExpenseData,
                     child: const Text('Save Expense'),
                   ),
                 ],
