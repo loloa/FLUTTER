5d18aa473cfe65caf941fe219e782af10dafb7e3
Lilia Lashin <lilial@crow.co.il>
2025-04-23 09:45:08 +0300

    Dynamic main content, popup on empty result, add/remove item with swipe and undo action.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -40,14 +40,48 @@ class _ExpensesState extends State<Expenses> {
     );
   }
 
+  // MARK:
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
     });
   }
 
+  void _removeExpense(Expense expense) {
+    final index = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense ${expense.title} deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(index, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No Expenses found. Start adding one!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
@@ -57,10 +91,7 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: Column(
-        children: [
-          const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
-        ],
+        children: [const Text('The chart'), Expanded(child: mainContent)],
       ),
     );
   }
