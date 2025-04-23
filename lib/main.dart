d5e3511a1473f7ecf661972892ad67d22c6ee10e
Lilia Lashin <lilial@crow.co.il>
2025-04-23 13:05:51 +0300

    dark theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,45 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 0, 158, 243),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromRGBO(125, 5, 79, 1),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      // DARK
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            color: kDarkColorScheme.onSecondaryContainer,
+            fontWeight: FontWeight.normal,
+          ),
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kDarkColorScheme.onPrimaryContainer,
+            fontSize: 40,
+          ),
+        ),
+      ),
+      // LIGHT
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -28,16 +64,17 @@ void main() {
         textTheme: ThemeData().textTheme.copyWith(
           bodyMedium: TextStyle(
             fontSize: 16,
-            color: kColorScheme.primary,
+            color: kColorScheme.onSecondaryContainer,
             fontWeight: FontWeight.normal,
           ),
           titleLarge: TextStyle(
             fontWeight: FontWeight.bold,
-            color: kColorScheme.onSecondaryContainer,
+            color: kColorScheme.onPrimaryContainer,
             fontSize: 40,
           ),
         ),
       ),
+
       home: Expenses(),
     ),
   );
