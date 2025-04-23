ad64f180264aa14f92e76834a12163e436b97e07
Lilia Lashin <lilial@crow.co.il>
2025-04-23 15:21:17 +0300

    Enabled orientation

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,74 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  runApp(
+    MaterialApp(
+      // DARK
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            color: kDarkColorScheme.onSecondaryContainer,
+            fontWeight: FontWeight.normal,
+          ),
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kDarkColorScheme.onPrimaryContainer,
+            fontSize: 40,
+          ),
+        ),
+      ),
+      // LIGHT
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primary,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.secondaryContainer,
+          ),
+        ),
+        // changes for all ui tetxs widgets by default
+        textTheme: ThemeData().textTheme.copyWith(
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            color: kColorScheme.onSecondaryContainer,
+            fontWeight: FontWeight.normal,
+          ),
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onPrimaryContainer,
+            fontSize: 40,
+          ),
+        ),
+      ),
+
+      home: Expenses(),
+    ),
+  );
+
+  /*
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
@@ -83,74 +151,5 @@ void main() {
         home: Expenses(),
       ),
     );
-  });
-  /*
-
-  runApp(
-    MaterialApp(
-      // DARK
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kDarkColorScheme.primaryContainer,
-          foregroundColor: kDarkColorScheme.onPrimaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-          bodyMedium: TextStyle(
-            fontSize: 16,
-            color: kDarkColorScheme.onSecondaryContainer,
-            fontWeight: FontWeight.normal,
-          ),
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kDarkColorScheme.onPrimaryContainer,
-            fontSize: 40,
-          ),
-        ),
-      ),
-      // LIGHT
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.primary,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.secondaryContainer,
-          ),
-        ),
-        // changes for all ui tetxs widgets by default
-        textTheme: ThemeData().textTheme.copyWith(
-          bodyMedium: TextStyle(
-            fontSize: 16,
-            color: kColorScheme.onSecondaryContainer,
-            fontWeight: FontWeight.normal,
-          ),
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kColorScheme.onPrimaryContainer,
-            fontSize: 40,
-          ),
-        ),
-      ),
-
-      home: Expenses(),
-    ),
-  );
-*/
+  });*/
 }
