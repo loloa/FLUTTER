e6f100b12f7d568846a6878c8840b69711e7f217
Lilia Lashin <lilial@crow.co.il>
2025-05-11 14:15:50 +0300

    .animate method to add curve

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -79,8 +79,14 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
       builder:
           (context, child) => SlideTransition(
-            position: _animationController.drive(
-              Tween(begin: const Offset(0, 0.3), end: const Offset(0, 0)),
+            position: Tween(
+              begin: const Offset(0, 0.3),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut, // Smooth easing curve
+              ),
             ),
             child: child,
           ),
